937cabc094e509ce971a3fa078ce79f8c60d7a46
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2019-06-30 19:37:11 -0500

    Let's think about curly braces later

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -3,12 +3,12 @@ import { Container } from './hero.css';
 
 const Hero = () => {
   return <Container>
-    <div style={{ fontSize: '2.5em', position: 'relative', left: '-120px', top: '-.75em', zIndex: '-1', fontWeight: '400i' }}>{'{'}</div>
+    {/* <div style={{ fontSize: '2.5em', position: 'relative', left: '-120px', top: '-.75em', zIndex: '-1', fontWeight: '400i' }}>{'{'}</div> */}
     <div>
       <div style={{ fontStyle: 'italic', fontWeight: '700' }}>Diego Benjamín</div>
       <div>Aguilar Aguilar</div>
     </div>
-    <div style={{ fontSize: '2.5em', position: 'relative', left: '120px', top: '.75em', zIndex: '-1' }}>{'}'}</div>
+    {/* <div style={{ fontSize: '2.5em', position: 'relative', left: '120px', top: '.75em', zIndex: '-1' }}>{'}'}</div> */}
   </Container>;
 };
 

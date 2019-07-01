7dd2b0711639067f3b448af76c377ab995877277
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2019-06-30 19:32:36 -0500

    Hero component

---
+++ src/components/hero/hero.css.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  color: rgb(16, 16, 16);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Playfair Display', serif;
+  height: 100vh;
+  font-size: 7em;
+  font-weight: 400;
+`;

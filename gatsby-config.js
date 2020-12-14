6c614fcc0dc7906bfb29d8e5fe405b0d81b4a607
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2020-12-14 17:41:56 -0600

    No PWA required

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,6 @@ module.exports = {
 	},
 	plugins: [
 		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `gatsby-starter-texblog`,
-				short_name: `texblog`,
-				start_url: `/`,
-				// Color settings can be found in /styles/colors.scss
-				background_color: `oldlace`,
-				display: `standalone`
-			},
-		},
-		{
 			resolve: `gatsby-plugin-google-analytics`,
 			options: {
 				// If using cloud deployment services, add GOOGLE_ANALYTICS_TRACKING_ID to your environment variables.
